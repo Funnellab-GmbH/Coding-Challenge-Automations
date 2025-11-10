cedf80d5e2d5140fb544eda04f9f0ce8b38593ab
Isabelle Glasmacher <isabelle.glasmacher@funnellab.de>
2025-11-10 09:03:27 +0100

    use correct data structure

--- api/src/createOrder.ts
+++ api/src/createOrder.ts
@@ -1,6 +1,14 @@
 import type { Request } from "express";
-import type { OrderItem } from "./data";
+import type { Order } from "./data";
 
-export const createOrder = (req: Request): OrderItem => {
-    return { label: "Sample Item", quantity: 1, unit_price: 100  };
+export const createOrder = (req: Request): Order => {
+    return {
+        external_order_id: "sample-order-id",
+        customer: { email: "john.doe@example.com", country: "US" },
+        line_items: [
+            { sku: "prod-1", quantity: 2, unit_price: 50.0 },
+            { sku: "prod-2", quantity: 1, unit_price: 50.0 }
+        ],
+        requested_at: '2025-02-15T12:11:22Z'
+    };
 };
--- api/src/data.ts
+++ api/src/data.ts
@@ -1,3 +1,10 @@
-export type OrderItem = { label: string; quantity: number; unit_price: number; };
+export type OrderItem = { sku: string; quantity: number; unit_price: number; };
+export type Order = {
+  external_order_id: string;
+  customer?: { email?: string; country?: string };
+  line_items: OrderItem[];
+  requested_at?: string;
+  status?: string;
+};
 
-export const ordersMap = new Map<string, any>();
+export const ordersMap = new Map<string, Order>();
